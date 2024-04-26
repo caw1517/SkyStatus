53cf76fd59d02c1b9b7cf22a8f97846d3f3e5d3c
Canaan White <canaan.white5@gmail.com>
2024-04-26 14:38:48 -0600

    Initial commit

---
+++ Backend/src/main/java/com/wpf/SkyStatus/SkyStatusApplication.java
@@ -0,0 +1,13 @@
+package com.wpf.SkyStatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyStatusApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/wpf/SkyStatus/SkyStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wpf.SkyStatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
